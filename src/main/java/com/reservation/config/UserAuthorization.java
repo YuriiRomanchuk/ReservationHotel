426d69abf00fa77b0ccf909b773b3253c268d209
yromanchuk <anybis2007@gmail.com>
2019-05-17 12:07:49 +0300

    user authorization

---
+++ src/main/java/com/reservation/config/UserAuthorization.java
@@ -0,0 +1,41 @@
+package com.reservation.config;
+
+import com.reservation.model.enums.Role;
+
+import java.util.Objects;
+
+public class UserAuthorization {
+    private String email;
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAuthorization that = (UserAuthorization) o;
+        return Objects.equals(email, that.email) &&
+                role == that.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, role);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+}
