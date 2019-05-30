9b7e3aa31847d6acd4c52a09e208fee3326ebb10
yromanchuk <anybis2007@gmail.com>
2019-05-30 22:17:53 +0300

    entity room and enum apartment class

--- src/main/java/com/reservation/model/entity/Room.java
+++ src/main/java/com/reservation/model/entity/Room.java
@@ -1,4 +1,11 @@
 package com.reservation.model.entity;
 
 public class Room {
+
+    private int id;
+    private int placeNumber;
+    private int apartmentСlass;
+
+
+
 }
---
+++ src/main/java/com/reservation/model/enums/ApartmentСlass.java
@@ -0,0 +1,5 @@
+package com.reservation.model.enums;
+
+public enum ApartmentСlass {
+    ECONOMY, COMFORT, BUSINESS, PREMIUM
+}
