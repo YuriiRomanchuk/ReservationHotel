a1208e12aeeff788319de76e74b6397a07098d8e
yromanchuk <anybis2007@gmail.com>
2019-06-03 15:57:37 +0300

    add invoice sql table and entity field

--- src/main/java/com/reservation/model/entity/Invoice.java
+++ src/main/java/com/reservation/model/entity/Invoice.java
@@ -3,12 +3,16 @@ package com.reservation.model.entity;
 import com.reservation.model.dto.RequestRoomDto;
 import com.reservation.model.dto.RoomDto;
 
+import java.util.Date;
+
 public class Invoice {
 
     private int id;
     private User user;
     private RequestRoomDto requestRoomDto;
     private RoomDto roomDto;
-    private int TotalPrice;
+    private Date arrivalDate;
+    private Date departureDate;
+    private int totalPrice;
 
 }
--- src/main/resources/sql/dataBaseInitializer.sql
+++ src/main/resources/sql/dataBaseInitializer.sql
@@ -36,66 +36,22 @@ create table if not exists request_rooms
     REFERENCES users (id)
 );
 
-
-/*create table if not exists films
+create table if not exists invoices
 (
-  id                  SERIAL       NOT NULL,
-  name                varchar(250) NOT NULL,
-  name_english        varchar(250) NOT NULL,
-  release_date        timestamp    NOT NULL,
-  description         varchar(250) NOT NULL,
-  description_english varchar(250) NOT NULL,
-  running_time        int          NOT NULL,
-  PRIMARY KEY (id)
-);
-
-create table if not exists rooms
-(
-  id           SERIAL       NOT NULL,
-  name         varchar(250) NOT NULL,
-  name_english varchar(250) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-create table if not exists places
-(
-  id      SERIAL NOT NULL,
-  row     int    NOT NULL,
-  place   int    NOT NULL,
-  room_id int    NOT NULL,
+  id               SERIAL    NOT NULL,
+  room_id          int       NOT NULL,
+  request_rooms_id int       NOT NULL,
+  user_id          int       NOT NULL,
+  arrival_date     timestamp NOT NULL,
+  departure_date   timestamp NOT NULL,
+  total_price      int       NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT places_room_id_fkey FOREIGN KEY (room_id)
-    REFERENCES rooms (id)
-);
-
-create table if not exists session
-(
-  id      SERIAL    NOT NULL,
-  film_id int       NOT NULL,
-  room_id int       NOT NULL,
-  date    timestamp NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT session_film_id_fkey FOREIGN KEY (film_id)
-    REFERENCES films (id),
-  CONSTRAINT session_room_id_fkey FOREIGN KEY (room_id)
-    REFERENCES rooms (id)
-);
-
-create table if not exists tickets
-(
-  id         SERIAL NOT NULL,
-  user_id    int    NOT NULL,
-  session_id int    NOT NULL,
-  place_id   int    NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT tickets_user_id_fkey FOREIGN KEY (user_id)
+  CONSTRAINT invoices_user_id_fkey FOREIGN KEY (user_id)
     REFERENCES users (id),
-  CONSTRAINT tickets_session_id_fkey FOREIGN KEY (session_id)
-    REFERENCES session (id),
-  CONSTRAINT tickets_place_id_fkey FOREIGN KEY (place_id)
-    REFERENCES places (id)
+  CONSTRAINT invoices_room_id_fkey FOREIGN KEY (room_id)
+    REFERENCES rooms (id),
+  CONSTRAINT invoices_request_rooms_id_fkey FOREIGN KEY (request_rooms_id)
+    REFERENCES request_rooms (id)
 );
-*/
-
 
 
