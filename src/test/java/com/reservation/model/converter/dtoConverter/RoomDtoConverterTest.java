d58add0271f98af1983c7dfa4360e8ff35128985
yromanchuk <anybis2007@gmail.com>
2019-06-07 22:25:34 +0300

    RoomDtoConverterTest and RoomServiceTest

--- src/test/java/com/reservation/model/converter/dtoConverter/RoomDtoConverterTest.java
+++ src/test/java/com/reservation/model/converter/dtoConverter/RoomDtoConverterTest.java
@@ -1,16 +1,57 @@
 package com.reservation.model.converter.dtoConverter;
 
+import com.reservation.model.dto.RoomDto;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import javax.servlet.http.HttpServletRequest;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class RoomDtoConverterTest {
+
+    @InjectMocks
+    private RoomDtoConverter roomDtoConverter;
 
     @Test
     public void convert() {
+
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        when(request.getParameter("placeNumber")).thenReturn("1");
+        when(request.getParameter("apartmentClass")).thenReturn("COMFORT");
+        when(request.getParameter("roomNumber")).thenReturn("105");
+        when(request.getParameter("price")).thenReturn("123123");
+
+        RoomDto roomDto = receiveRoomDtoTest();
+        RoomDto convert = roomDtoConverter.convert(request);
+
+        Assert.assertTrue(new ReflectionEquals(convert).matches(roomDto));
+
     }
 
     @Test
     public void convertByRoomId() {
+        RoomDto roomDto = new RoomDto();
+        int room_id = 1;
+
+        roomDto.setId(room_id);
+        RoomDto convert = roomDtoConverter.convertByRoomId(String.valueOf(room_id));
+        Assert.assertTrue(new ReflectionEquals(convert).matches(roomDto));
+    }
+
+    private RoomDto receiveRoomDtoTest() {
+        RoomDto roomDto = new RoomDto();
+        roomDto.setPlaceNumber(1);
+        roomDto.setApartmentClass("COMFORT");
+        roomDto.setRoomNumber(105);
+        roomDto.setPrice(123123);
+
+        return roomDto;
     }
 }
--- src/test/java/com/reservation/service/RoomServiceTest.java
+++ src/test/java/com/reservation/service/RoomServiceTest.java
@@ -1,20 +1,72 @@
 package com.reservation.service;
 
+import com.reservation.exception.ServiceException;
+import com.reservation.model.converter.dtoConverter.RoomDtoFromEntityConverter;
+import com.reservation.model.converter.entityConverter.RoomConverter;
+import com.reservation.model.dao.RoomDao;
+import com.reservation.model.dto.RoomDto;
+import com.reservation.model.entity.Room;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class RoomServiceTest {
 
+    @Mock
+    private RoomDao roomDao;
+
+    @Mock
+    private RoomConverter roomConverter;
+
+    @Mock
+    private RoomDtoFromEntityConverter roomDtoFromEntityConverter;
+
+    @InjectMocks
+    private RoomService roomService;
+
     @Test
-    public void createRoom() {
+    public void createRoom() throws ServiceException {
+        RoomDto roomDto = new RoomDto();
+        Room room = new Room();
+        when(roomConverter.convert(roomDto)).thenReturn(room);
+        roomService.createRoom(roomDto);
+        verify(roomDao).insert(room);
     }
 
     @Test
-    public void receiveAllRoomsDto() {
+    public void receiveAllRoomsDto() throws ServiceException {
+        RoomDto roomDto1 = new RoomDto();
+        RoomDto roomDto2 = new RoomDto();
+        Room room1 = new Room();
+        Room room2 = new Room();
+
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(room1);
+        rooms.add(room2);
+
+        when(roomDao.findAll()).thenReturn(rooms);
+        when(roomDtoFromEntityConverter.convert(room1)).thenReturn(roomDto1);
+        when(roomDtoFromEntityConverter.convert(room2)).thenReturn(roomDto2);
+
+        List<RoomDto> roomsDto = roomService.receiveAllRoomsDto();
+        assertEquals(2, roomsDto.size());
+
+        assertEquals(roomDto1, roomsDto.get(0));
+        assertEquals(roomDto2, roomsDto.get(1));
     }
 
     @Test
     public void receiveFreeRoomsByParameters() {
+
     }
 }
