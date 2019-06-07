f0cadb5b55cf7efcdd06fd225c2de6697aabf5cc
yromanchuk <anybis2007@gmail.com>
2019-06-07 22:25:28 +0300

    RoomDtoConverterTest and RoomServiceTest

---
+++ src/test/java/com/reservation/model/converter/dtoConverter/RoomDtoConverterTest.java
@@ -0,0 +1,16 @@
+package com.reservation.model.converter.dtoConverter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoomDtoConverterTest {
+
+    @Test
+    public void convert() {
+    }
+
+    @Test
+    public void convertByRoomId() {
+    }
+}
---
+++ src/test/java/com/reservation/service/RoomServiceTest.java
@@ -0,0 +1,20 @@
+package com.reservation.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoomServiceTest {
+
+    @Test
+    public void createRoom() {
+    }
+
+    @Test
+    public void receiveAllRoomsDto() {
+    }
+
+    @Test
+    public void receiveFreeRoomsByParameters() {
+    }
+}
